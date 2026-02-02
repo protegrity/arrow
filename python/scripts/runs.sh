9803971ecaf8ad1d3979e572f1cfc4919e6ce48f
Marco Arguedas <marco@tekdatum.com>
2026-02-01 18:01:08 -0600

    added simple-to-parse stats (csv)

---
+++ python/scripts/runs.sh
@@ -0,0 +1,48 @@
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=5 --input-file=random_names_100MM.txt --max-rows=1000000 
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=5 --input-file=random_names_100MM.txt --max-rows=5000000
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=5 --input-file=random_names_100MM.txt --max-rows=10000000 
+
+
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=5 --input-file=random_names_100MM.txt --max-rows=1000000 
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=5 --input-file=random_names_100MM.txt --max-rows=5000000
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=5 --input-file=random_names_100MM.txt --max-rows=10000000 
+
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=2 --input-file=random_names_100MM.txt --max-rows=1000000 
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=2 --input-file=random_names_100MM.txt --max-rows=5000000
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=2 --input-file=random_names_100MM.txt --max-rows=10000000
+
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=2 --input-file=random_names_100MM.txt --max-rows=1000000 
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=2 --input-file=random_names_100MM.txt --max-rows=5000000
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=2 --input-file=random_names_100MM.txt --max-rows=10000000
+
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=3 --input-file=random_names_100MM.txt --max-rows=1000000 
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=3 --input-file=random_names_100MM.txt --max-rows=5000000
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=3 --input-file=random_names_100MM.txt --max-rows=10000000
+
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=3 --input-file=random_names_100MM.txt --max-rows=1000000 
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=3 --input-file=random_names_100MM.txt --max-rows=5000000
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=3 --input-file=random_names_100MM.txt --max-rows=10000000 
+
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=6 --input-file=random_names_100MM.txt --max-rows=1000000 
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=6 --input-file=random_names_100MM.txt --max-rows=5000000
+# python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=6 --input-file=random_names_100MM.txt --max-rows=10000000
+
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=6 --input-file=random_names_100MM.txt --max-rows=1000000 
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=6 --input-file=random_names_100MM.txt --max-rows=5000000
+# python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=6 --input-file=random_names_100MM.txt --max-rows=10000000 
+
+#python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=4 --input-file=random_names_100MM.txt --max-rows=1000000 
+#python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=4 --input-file=random_names_100MM.txt --max-rows=5000000
+#python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=4 --input-file=random_names_100MM.txt --max-rows=10000000 
+
+
+#DBPS_FORCE_PER_BLOCK_ENCRYPTION=true python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=1 --input-file=random_names_100MM.txt --max-rows=1000000
+#DBPS_FORCE_PER_BLOCK_ENCRYPTION=true python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=1 --input-file=random_names_100MM.txt --max-rows=5000000
+#DBPS_FORCE_PER_BLOCK_ENCRYPTION=true python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=1 --input-file=random_names_100MM.txt --max-rows=10000000
+
+#DBPS_FORCE_PER_BLOCK_ENCRYPTION=true python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=4 --input-file=random_names_100MM.txt --max-rows=1000000 
+#DBPS_FORCE_PER_BLOCK_ENCRYPTION=true python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=4 --input-file=random_names_100MM.txt --max-rows=5000000
+
+DBPS_FORCE_PER_BLOCK_ENCRYPTION=true python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=5 --input-file=random_names_100MM.txt --max-rows=1000000 
+DBPS_FORCE_PER_BLOCK_ENCRYPTION=true python3 ./baselining_app.py --warmup=1 --iterations=200  --scenario=5 --input-file=random_names_100MM.txt --max-rows=5000000
+DBPS_FORCE_PER_BLOCK_ENCRYPTION=true python3 ./baselining_app.py --warmup=1 --iterations=100  --scenario=5 --input-file=random_names_100MM.txt --max-rows=10000000
